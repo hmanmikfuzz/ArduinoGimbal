08469a932ad9d2597c5c664ffb4fb4504d98861a
hmanmikfuzz <harrison.sneck@gmail.com>
2016-03-30 11:27:43 -0400

    new broken

--- Arduino_Gimbal/Arduino_Gimbal.ino
+++ Arduino_Gimbal/Arduino_Gimbal.ino
@@ -8,24 +8,42 @@ class GimbalAxis {
   
   
 public: 
-   GimbalAxis(int servoPin, int potpin, int potval, int a){
+   GimbalAxis(int servoPin, int potpin){
      Servo servo;
      servo.attach(servoPin);
      potPin = potpin;
-     potVal = potval;
-     angle = a;    
+     updateGimbal();  
   }
 
-  
+  int getPotVal(){
+    return potVal;
+  }
+
+  int getAngle(){
+    return angle;
+  }
+
+  void updateGimbal(){
+    potVal = analogRead(potPin);
+    angle = map(potVal, 0, 1023 ,0,179);
+    servo.write(angle);
+  }
 };
+
+GimgbalAxis x;
 
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
+ // x = new GimbalAxis(2,A0);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  x.updateGimgal();
+  Serial.print("x potVal: ");
+  Serial.print(x.getPotVal());
+  Serial.print(" angle: ");
+  Serial.println(x.getAngle);
 
 }
